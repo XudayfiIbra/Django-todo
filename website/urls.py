e96afe71da72e35fed889d13c2d0282d2194b256
xudayfi <xudayfiibraahim8@gmail.com>
2024-04-28 17:38:08 +0300

    todo 1

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+# if you have more than one app and links get same name use 
+# app_name and then give a name then in the link do this
+# url "app_name:url"
+# app_name = 'task'
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('add', views.add, name='add')
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def test(request):
-    pass
-    
+
+tasks = ['Go shopping', 'Do homework', 'Go to the GYM']
+def index(request):
+    return render(request, 'index.html', {
+        'tasks': tasks
+    })
+
+
+def add(request):
+    return render(request, 'add.html')
     
