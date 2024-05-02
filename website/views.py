86f2c329083cfface5bf9edfdc511488457354f6
xudayfi <xudayfiibraahim8@gmail.com>
2024-05-02 04:08:03 +0300

    forms

--- website/views.py
+++ website/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+from django.urls import reverse
+
+class NewTask(forms.Form):
+    task = forms.CharField(label="Add a task",max_length=300)
+    
 
 
-tasks = ['Go shopping', 'Do homework', 'Go to the GYM']
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, 'index.html', {
-        'tasks': tasks
+        'tasks': request.session["tasks"]
     })
 
 
 def add(request):
-    return render(request, 'add.html')
+    if request.method == 'POST':
+        form = NewTask(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task] 
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'add.html', {
+                'form': form
+            })
+    return render(request, 'add.html', {
+        'form': NewTask
+    })
     
